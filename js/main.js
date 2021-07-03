628491ea55344e7cbef0ff0fbda7138b47d7b376
Ashtrath <69681505+Dyzean@users.noreply.github.com>
2021-07-03 12:24:22 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,1 @@
-document.body.onmousemove = function(e) {
-  document.documentElement.style.setProperty (
-    '--x', (
-      e.clientX+window.scrollX
-    )
-    + 'px'
-  );
-  document.documentElement.style.setProperty (
-    '--y', (
-      e.clientY+window.scrollY
-    ) 
-    + 'px'
-  );
-}
+<!-- Empty for now -->
